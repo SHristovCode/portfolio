67a887365022f9a955c4f1f3a5435fb7d98f175b
Svilen Hristov <s.hristov@infoleven.com>
2020-04-16 17:08:30 +0300

    text changes

--- src/components/SliderTextImgSection/SliderTextImgSection.js
+++ src/components/SliderTextImgSection/SliderTextImgSection.js
@@ -24,17 +24,17 @@ const SliderTextImgSection = () => {
                     <ImageText
                         image="ImageKeyboard"
                         headline="I never stop learning"
-                        text="I always improve myself, because I enjoy this job and I want to achive a lot with it. I never stop to be in touch with the news."
+                        text="I always improve myself, because I enjoy this job and I want to achive a lot with it. I never stop being touch with the news."
                     />
                     <ImageText
                         image="Desk"
                         headline="I'm looking for challenges"
-                        text="I'm looking for challenges that will improve my skills and will make me better developer. I always try to put myself in hard situation just to find the correct answer."
+                        text="I'm looking for challenges that will improve my skills and will make me a better developer. I always try to put myself in hard situation just to find the correct answer."
                     />
                     <ImageText
                         image="DeskSecond"
                         headline="I want to achieve as much as it possible"
-                        text="I'm motivated person which want to become expert in front end development. This is my main target which I won't stop following."
+                        text="I'm a motivated person which wants to become an expert in front end development. This is my main target which I won't stop following."
                     />
                 </SimpleSlider>
             </div>
